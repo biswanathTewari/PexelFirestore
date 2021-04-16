9d925509b4c1befb01542b4e508af5bd2b509e41
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-04-16 19:44:24 +0530

    setup - file structure , firebase

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+import "../styles/app.scss";
+
+const App = () => {
+	return <div className="App">hello</div>;
+};
+
+export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore"
+import "firebase/storage"
+
+var firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const projStorage = firebase.storage()
+export const db = firebase.firestore()
+export default firebase;
